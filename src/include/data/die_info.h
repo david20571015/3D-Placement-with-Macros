c97a56371c44435791c0c2545f965847f3b5011b
Tzu-Hao Lin <ok10155096@gmail.com>
2023-07-09 19:27:46 +0800

    implement func.

--- src/include/data/die_info.h
+++ src/include/data/die_info.h
@@ -25,6 +25,8 @@ struct LibCell {
 
   std::vector<Pin> pins;
 
+  int get_cell_size() const { return size.x * size.y; };
+
   friend std::istream& operator>>(std::istream& input, LibCell& lib_cell);
 };
 
@@ -32,6 +34,8 @@ struct Technology {
   std::string tech_name;
   int lib_cell_count;
   std::vector<LibCell> lib_cells;
+
+  int get_lib_cell_index(std::string lib_cell_name) const;
 
   friend std::istream& operator>>(std::istream& input, Technology& tech);
 };
