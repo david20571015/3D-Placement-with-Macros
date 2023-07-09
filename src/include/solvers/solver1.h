f05f9d677321c59056b5e74408404340b9310279
Tzu-Hao Lin <ok10155096@gmail.com>
2023-07-09 19:28:38 +0800

    add vars. & func.

--- src/include/solvers/solver1.h
+++ src/include/solvers/solver1.h
@@ -1,8 +1,13 @@
 #include "solver.h"
 
 class Solver1 : public Solver {
+  std::vector<int> die_max_util;
+  std::vector<int> die_util;
+  int die_size;
+  
  public:
   explicit Solver1(Case& case_);
   ~Solver1() override = default;
   void solve() override;
+  bool check_capacity(int index, int die_cell_index);
 };
